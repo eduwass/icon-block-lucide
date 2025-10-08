448dea4f1c04268ef389821316cd7c08c4d408e9
Edu Wass <eduwass@gmail.com>
2025-10-08 07:03:08 +0200

    hello

---
+++ icon-block-lucide.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       Icon Block Lucide
+ * Description:       Adds Lucide icon set to the Icon Block.
+ * Requires at least: 6.5
+ * Requires PHP:      7.4
+ * Version:           1.0.0
+ * Author:            Edu Wass
+ * Author URI:        https://eduwass.com
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       icon-block-lucide
+ */
