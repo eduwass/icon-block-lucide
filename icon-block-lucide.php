d800892c77ecdfe446df57e51894dd41d7db80ca
Edu Wass <eduwass@gmail.com>
2025-10-08 07:18:44 +0200

    register

--- icon-block-lucide.php
+++ icon-block-lucide.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:       Icon Block Lucide
  * Description:       Adds Lucide icon set to the Icon Block.
@@ -11,3 +12,27 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       icon-block-lucide
  */
+
+/**
+ * Prevent direct access.
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Version number.
+ */
+const ICON_BLOCK_LUCIDE_VERSION = '1.0.0';
+
+/**
+ * Registers the custom icons for the Icon Block.
+ */
+function icon_block_lucide_register_custom_icons() {
+    wp_enqueue_script(
+        'icon-block-lucide-register-custom-icons',
+        plugin_dir_url(__FILE__) . 'register.js',
+        array('wp-i18n', 'wp-hooks', 'wp-dom'),
+        ICON_BLOCK_LUCIDE_VERSION,
+        true
+    );
+}
+add_action('enqueue_block_editor_assets', 'icon_block_lucide_register_custom_icons');
